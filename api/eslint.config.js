67bb5dbd12c68228430601d704b7773bbeeddee1
Kristen Mills <kristen@kristen-mills.com>
2025-03-30 14:13:48 -0700

    Use npm workspaces

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -2,23 +2,21 @@ import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: { ...globals.node } },
   },
   {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     plugins: { js },
     extends: ['js/recommended'],
   },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
 ]);
