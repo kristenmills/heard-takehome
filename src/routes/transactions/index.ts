c7f3dbf3b58f0096f70d5bfb5c09f766eaab5059
Kristen Mills <kristen@kristen-mills.com>
2025-03-30 13:17:36 -0700

    Stub out transaction routes

--- src/routes/transactions/index.ts
+++ src/routes/transactions/index.ts
@@ -1,7 +1,23 @@
 import { FastifyPluginAsync } from 'fastify';
 
-const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+const example: FastifyPluginAsync = async (fastify) => {
   fastify.get('/', async function (request, reply) {
+    return 'this is an example';
+  });
+
+  fastify.post('/', async (request, reply) => {
+    return 'this is an example';
+  });
+
+  fastify.get('/:transactionId', async (request, reply) => {
+    return 'this is an example';
+  });
+
+  fastify.put('/:transactionId', async (request, reply) => {
+    return 'this is an example';
+  });
+
+  fastify.delete('/:transactionId', async (request, reply) => {
     return 'this is an example';
   });
 };
