4cd4d83247d779e883a15a284b9b6f549cd7ed2b
Kristen Mills <kristen@kristen-mills.com>
2025-03-30 13:56:33 -0700

    Base implementation of the crud routes

--- src/routes/transactions/index.ts
+++ src/routes/transactions/index.ts
@@ -1,24 +1,74 @@
 import { FastifyPluginAsync } from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+interface TransactionRequest {
+  description: string;
+  transactionId: string;
+  amount: number;
+  fromAccount: string;
+  toAccount: string;
+}
+
+interface Params {
+  transactionId: string;
+}
 
 const example: FastifyPluginAsync = async (fastify) => {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example';
+  fastify.get('/', async () => {
+    const transactions = await prisma.transaction.findMany();
+    return transactions;
   });
 
-  fastify.post('/', async (request, reply) => {
-    return 'this is an example';
+  fastify.post<{
+    Body: TransactionRequest;
+  }>('/', async (req) => {
+    const { transactionId, description, amount, fromAccount, toAccount } =
+      req.body;
+    const transaction = await prisma.transaction.create({
+      data: {
+        description,
+        transactionId,
+        amount,
+        fromAccount,
+        toAccount,
+      },
+    });
+    return transaction;
   });
 
-  fastify.get('/:transactionId', async (request, reply) => {
-    return 'this is an example';
+  fastify.get<{ Params: Params }>('/:transactionId', async (req) => {
+    const { transactionId } = req.params;
+    const transaction = await prisma.transaction.findUnique({
+      where: { transactionId },
+    });
+    return transaction;
   });
 
-  fastify.put('/:transactionId', async (request, reply) => {
-    return 'this is an example';
+  fastify.put<{
+    Body: TransactionRequest;
+    Params: Params;
+  }>('/:transactionId', async (req) => {
+    const { transactionId } = req.params;
+    const { amount, fromAccount, toAccount, description } = req.body;
+    const transaction = await prisma.transaction.update({
+      where: { transactionId },
+      data: {
+        description,
+        amount,
+        fromAccount,
+        toAccount,
+      },
+    });
+    return transaction;
   });
 
-  fastify.delete('/:transactionId', async (request, reply) => {
-    return 'this is an example';
+  fastify.delete<{ Params: Params }>('/:transactionId', async (req) => {
+    const { transactionId } = req.params;
+    await prisma.transaction.delete({
+      where: { transactionId },
+    });
   });
 };
 
